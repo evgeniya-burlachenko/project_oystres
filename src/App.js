0806d36ca3f7c209e9136998614c0762b741fc2d
unknown <e.accounte@gmail.com>
2020-10-09 22:51:34 +0300

    поменяла fetch на axios для ознакомления

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import {Header} from "./components";
 import {Cart,Home} from "./pages";
@@ -8,12 +9,17 @@ import {Route} from "react-router-dom";
 function App() {
     const  [oysters, setOysters] = React.useState([]);
 
-React.useEffect(() =>{
-fetch('http://localhost:3000/db.json')
-    .then((resp) => resp.json())
-    .then((json) => {
-        setOysters(json.oysters);
-    });
+React.useEffect(() => {
+    /*fetch('http://localhost:3000/db.json')
+        .then((resp) => resp.json())
+        .then((json) => {
+            setOysters(json.oysters);
+        });
+    },[])*/
+    axios.get('http://localhost:3000/db.json')
+        .then(({data})=>{
+            setOysters(data.oysters);
+            });
 },[])
 
   return (
